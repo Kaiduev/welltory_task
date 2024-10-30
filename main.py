261ed5c2201c93294b8e4cf2ddd9dc410760807d
Kaiduev <kaiduevbaktygul07@gmail.com>
2024-10-30 18:20:55 +0600

    some changes

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, Float, DateTime, ForeignKey, select, func
-from sqlalchemy.orm import sessionmaker
-from datetime import datetime
-from sqlalchemy.exc import SQLAlchemyError
 import logging
+from datetime import datetime
+from typing import List, Tuple
+
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, Float, DateTime, ForeignKey, select, \
+    func
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.orm import sessionmaker
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-
-engine = create_engine('postgresql://username:password@host:port/database_name')
-
+DATABASE_URL = 'postgresql://username:password@host:port/database_name'
+engine = create_engine(DATABASE_URL)
 metadata = MetaData()
 
 users = Table(
@@ -26,59 +28,54 @@ heart_rates = Table(
     metadata,
     Column('id', Integer, primary_key=True),
     Column('user_id', Integer, ForeignKey('users.id'), index=True),
-    Column('timestamp', DateTime),
+    Column('timestamp', DateTime, index=True),
     Column('heart_rate', Float),
 )
 
 metadata.create_all(engine)
-
 Session = sessionmaker(bind=engine)
 
 
-def query_users(min_age, gender, min_avg_heart_rate, date_from, date_to):
-    try:
+def log_sqlalchemy_error(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except SQLAlchemyError as e:
+            logger.error(f"An SQLAlchemy error has occurred: {e}")
+            return {"error": f"An error occurred while executing the request: {e}"}
+    return wrapper
 
-        session = Session()
 
-        avg_heart_rate_subquery = (
+@log_sqlalchemy_error
+def query_users(min_age: int, gender: str, min_avg_heart_rate: float, date_from: datetime, date_to: datetime) -> List[Tuple]:
+    with Session() as session:
+        avg_heart_rate = (
             select(
                 heart_rates.c.user_id,
                 func.avg(heart_rates.c.heart_rate).label('avg_heart_rate')
             )
             .where(
-                heart_rates.c.timestamp >= date_from,
-                heart_rates.c.timestamp <= date_to
+                heart_rates.c.timestamp.between(date_from, date_to)
             )
             .group_by(heart_rates.c.user_id)
-            .alias('avg_heart_rate_subquery')
-        )
+        ).alias('avg_heart_rate_subquery')
 
         stmt = (
             select(users)
-            .select_from(users.join(avg_heart_rate_subquery, users.c.id == avg_heart_rate_subquery.c.user_id))
+            .join(avg_heart_rate, users.c.id == avg_heart_rate.c.user_id)
             .where(
                 func.cast(users.c.age, Integer) > min_age,
                 users.c.gender == gender,
-                avg_heart_rate_subquery.c.avg_heart_rate > min_avg_heart_rate
+                avg_heart_rate.c.avg_heart_rate > min_avg_heart_rate
             )
         )
 
-        result = session.execute(stmt).fetchall()
+        return session.execute(stmt).fetchall()
 
-        session.close()
 
-        return result
-
-    except SQLAlchemyError as e:
-        logger.error(f"An SQLAlchemy error has occurred: {e}")
-        return {"error": f"An error occurred while executing the request: {e}"}
-    
-    
-def query_for_user(user_id, date_from, date_to):
-    try:
-
-        session = Session()
-
+@log_sqlalchemy_error
+def query_for_user(user_id: int, date_from: datetime, date_to: datetime) -> List[Tuple]:
+    with Session() as session:
         subquery = (
             select(
                 func.avg(heart_rates.c.heart_rate).label('avg_heart_rate'),
@@ -86,26 +83,12 @@ def query_for_user(user_id, date_from, date_to):
             )
             .where(
                 heart_rates.c.user_id == user_id,
-                heart_rates.c.timestamp >= date_from,
-                heart_rates.c.timestamp <= date_to
+                heart_rates.c.timestamp.between(date_from, date_to)
             )
             .group_by(func.date_trunc('hour', heart_rates.c.timestamp))
             .order_by(func.avg(heart_rates.c.heart_rate).desc())
             .limit(10)
-            .alias('subquery')
         )
 
-        stmt = (
-            select(subquery.c.avg_heart_rate)
-            .order_by(subquery.c.hour)
-        )
-
-        result = session.execute(stmt).fetchall()
-
-        session.close()
-
-        return result
-
-    except SQLAlchemyError as e:
-        logger.error(f"An SQLAlchemy error has occurred: {e}")
-        return {"error": f"An error occurred while executing the request: {e}"}
+        stmt = select(subquery.c.avg_heart_rate).order_by(subquery.c.hour)
+        return session.execute(stmt).fetchall()
